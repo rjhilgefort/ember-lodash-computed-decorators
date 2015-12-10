4d29ef00631670fef36c569f3fde686d63677af2
Rob Hilgefort <rjhilgefort@gmail.com>
2015-12-10 15:42:06 -0500

    add all the new methods

--- addon/index.js
+++ addon/index.js
@@ -1,4 +1,20 @@
 import macroAlias from 'ember-computed-decorators/macro-alias';
 import * as computed from 'ember-lodash-computed';
 
+export var isArguments = macroAlias(computed.isArguments);
+export var isArray = macroAlias(computed.isArray);
+export var isBoolean = macroAlias(computed.isBoolean);
+export var isDate = macroAlias(computed.isDate);
+export var isElement = macroAlias(computed.isElement);
+export var isEmpty = macroAlias(computed.isEmpty);
+export var isFunction = macroAlias(computed.isFunction);
+export var isNaN = macroAlias(computed.isNaN);
+export var isNative = macroAlias(computed.isNative);
+export var isNull = macroAlias(computed.isNull);
+export var isNumber = macroAlias(computed.isNumber);
+export var isObject = macroAlias(computed.isObject);
+export var isPlainObject = macroAlias(computed.isPlainObject);
+export var isRegExp = macroAlias(computed.isRegExp);
 export var isString = macroAlias(computed.isString);
+export var isTypedArray = macroAlias(computed.isTypedArray);
+export var isUndefined = macroAlias(computed.isUndefined);
