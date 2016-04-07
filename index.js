553e492768f5f342396a644bd0884f6495462713
Nick Jutte <nick.jutte@zipscene.com>
2016-04-07 13:31:07 -0400

    Cleanup

--- index.js
+++ index.js
@@ -2,19 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-lodash-computed-decorators', /**,
+  name: 'ember-lodash-computed-decorators' /**,
 
   isDevelopingAddon: function(app) {
     return true;
   }
   **/
-  init: function(app) {
-    this.options = this.options || {};
-    this.options.babel = this.options.babel || {};
-    this.options.babel.optional = this.options.babel.optional || [];
-
-    if (this.options.babel.optional.indexOf('es7.decorators') === -1) {
-      this.options.babel.optional.push('es7.decorators');
-    }
-  }
 };
